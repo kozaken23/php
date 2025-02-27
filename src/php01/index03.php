cc9ba940e43a976ce34628323226b56d603dcdea
kozaken23 <xiaoqixianda@gmail.com>
2025-02-27 15:54:21 +0900

    リモートリポジトリの変更

---
+++ src/php01/index03.php
@@ -0,0 +1,13 @@
+<?php
+$name1 = "Sato" . "Taro";
+$name2 =  "Tanaka";
+$last_name = "Yamada";
+$first_name = "Saburo";
+
+echo $name1;
+echo "<br />";
+echo $name2 . "Jiro";
+echo "<br />";
+echo $last_name . $first_name;
+
+C:\Users\kento\coachtech\php\src\php01\index03.php
